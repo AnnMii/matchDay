4a0177124dee046dce89a7bc0da7e5be0a767afd
Andreas Mitzkus <andreasmitzkus@web.de>
2023-05-02 20:41:32 +0200

    Initial commit

---
+++ src/main/java/com/mitzkus/taso/matchDay/MatchDayApplication.java
@@ -0,0 +1,13 @@
+package com.mitzkus.taso.matchDay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchDayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mitzkus/taso/matchDay/MatchDayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mitzkus.taso.matchDay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
