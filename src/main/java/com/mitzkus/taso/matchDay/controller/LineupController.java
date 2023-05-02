eef6ddc65a4df5f9370bc8ad9cfcb0514ce6f0e2
Andreas Mitzkus <andreasmitzkus@web.de>
2023-05-02 21:59:44 +0200

    Add a first controller with two sample endpoints

--- src/main/java/com/mitzkus/taso/matchDay/controller/LineupController.java
+++ src/main/java/com/mitzkus/taso/matchDay/controller/LineupController.java
@@ -1,2 +1,18 @@
-package com.mitzkus.taso.matchDay.controller;public class LineupController {
+package com.mitzkus.taso.matchDay.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LineupController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @GetMapping("/getLineup")
+    public String getLineup() {
+        return "Hello from LineupController!";
+    }
 }
